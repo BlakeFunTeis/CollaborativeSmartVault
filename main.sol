b625f2d39e4182ec3e7cd41de36aebde978ef4e1
BlakeFunTeis <tk0wo3@gmail.com>
2023-02-05 13:05:33 +0800

    add: create owner

---
+++ main.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "hardhat/console.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+
+contract Main is Ownable{
+    
+}
---
+++ owner.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "hardhat/console.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract OwnerAction is Ownable {
+
+    struct Actions {
+        bool withdrawal;
+        bool deposit;
+    }
+
+    mapping (address => Actions) AddressAccess;
+
+    function setWithdrawalAccess(address _address, bool _toggle) public onlyOwner {
+        AddressAccess[_address].withdrawal = _toggle;
+    }
+
+    function setDepositAccess(address _address, bool _toggle) public onlyOwner {
+        AddressAccess[_address].deposit = _toggle;
+    }
+
+    function getWithdrawalAccess(address _address) public view onlyOwner returns(bool) {
+        return AddressAccess[_address].withdrawal;
+    }
+
+    function getDepositAccess(address _address) public view onlyOwner returns(bool) {
+        return AddressAccess[_address].deposit;
+    }
+}
